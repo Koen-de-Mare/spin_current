ab30b1a694e30ee49183fe04fa896f015a1f94c3
koen <k.a.d.mare@student.tue.nl>
2020-04-20 12:33:00 +0200

    hot electron current induces thermal electron current, plotting reflects new behaviour

--- src/animate.py
+++ src/animate.py
@@ -14,40 +14,57 @@ states = []
 
 num_frames = 200
 for i in range(num_frames):
+    print(i / num_frames)
     sys.step()
     #gamma_lists.append(sys.gamma_list.copy())
     states.append(sys.make_data())
 
-ticks = sys.make_ticks()
+(ticks_slices, ticks_planes) = sys.make_ticks()
 
 # code based on:
 # https://stackoverflow.com/questions/49165233/two-lines-matplotib-animation
 
-fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10, 8))
+fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(5, 1, figsize=(10, 8))
 
 ax1.set_xlim(0, sys.slice_length * sys.num_slices)
 ax1.set_ylim(-0.1, 0.1)
+ax1.axhline(y=0, color='g')
 line1, = ax1.plot([], [], color="r")
 
 ax2.set_xlim(0, sys.slice_length * sys.num_slices)
 ax2.set_ylim(-0.1, 0.1)
+ax2.axhline(y=0, color='g')
 line2, = ax2.plot([], [], color="b")
 line3, = ax2.plot([], [], color="y")
 
 ax3.set_xlim(0, sys.slice_length * sys.num_slices)
 ax3.set_ylim(-0.1, 0.1)
+ax3.axhline(y=0, color='g')
 line4, = ax3.plot([], [], color="b")
 line5, = ax3.plot([], [], color="y")
 
+ax4.set_xlim(0, sys.slice_length * sys.num_slices)
+ax4.set_ylim(0.0, 10.0)
+
+line6, = ax4.plot([], [], color="b")
+line7, = ax4.plot([], [], color="y")
+
+ax5.set_xlim(0, sys.slice_length * sys.num_slices)
+ax5.set_ylim(-1.0, 1.0)
+ax5.axhline(y=0, color='g')
+line8, = ax5.plot([], [], color="r")
 
 def update(n):
-    (gamma, mu0_up, mu0_dn, mu0_hot_up, mu0_hot_dn) = states[n]
-    line1.set_data(ticks, gamma)
-    line2.set_data(ticks, mu0_up)
-    line3.set_data(ticks, mu0_hot_up)
-    line4.set_data(ticks, mu0_dn)
-    line5.set_data(ticks, mu0_hot_dn)
-    return [line1, line2, line3, line4, line5]
+    (gamma, mu0_up, mu0_dn, hot_up, hot_dn, mu0_hot_up, mu0_hot_dn, j_hot, j_up, j_dn) = states[n]
+    line1.set_data(ticks_slices, gamma)
+    line2.set_data(ticks_slices, mu0_up)
+    line3.set_data(ticks_slices, mu0_hot_up)
+    line4.set_data(ticks_slices, mu0_dn)
+    line5.set_data(ticks_slices, mu0_hot_dn)
+    line6.set_data(ticks_slices, hot_up)
+    line7.set_data(ticks_slices, hot_dn)
+    line8.set_data(ticks_planes, j_hot)
+    return [line1, line2, line3, line4, line5, line6, line7, line8]
 
 
 anim = animation.FuncAnimation(fig, update, num_frames, interval=100, blit=True)
--- src/system.py
+++ src/system.py
@@ -29,6 +29,11 @@ class System:
         self.t = 0.0
         self.hot_list = []
 
+        # state dynamics
+        self.j_hot = []
+        self.j_up = []
+        self.j_dn = []
+
         # properties
         self.num_slices = 0
         self.slice_length = 0.0  # (nm)
@@ -39,10 +44,15 @@ class System:
     def step(self):
         self.t += self.dt
         xi = self.material.xi()
+        ds_tot = self.material.ds_up + self.material.ds_dn
+        alpha_tot = self.material.alpha_up + self.material.alpha_dn
 
         # transport of hot electrons -----------------------------------------------------------------------------------
+        self.j_hot = [0.0] * (self.num_slices - 1)
+
         for i in range(len(self.hot_list)):
             hot_electron = self.hot_list[i]
+            z0 = hot_electron.z
             hot_electron.z += hot_electron.vz * self.dt
             if hot_electron.z < 0.0:
                 hot_electron.z = -hot_electron.z
@@ -50,27 +60,58 @@ class System:
             if hot_electron.z > self.slice_length * self.num_slices:
                 hot_electron.z = 2 * self.slice_length * self.num_slices - hot_electron.z
                 hot_electron.vz = -hot_electron.vz
+            z1 = hot_electron.z
+
+            zmin = min(z0, z1)
+            jmin = math.ceil(zmin / self.slice_length - 1.0)
+            zmax = max(z0, z1)
+            jmax = math.ceil(zmax / self.slice_length - 1.0)
+
+            assert(jmin >= 0)
+            assert(jmax < self.num_slices)
+
+            sign = 0
+            if z1 > z0:
+                sign = 1.0
+            else:
+                sign = -1.0
+
+            for j in range(jmin, jmax):
+                self.j_hot[j] += sign * electrons_per_packet
+
 
         # motion of thermal electrons ----------------------------------------------------------------------------------
-
-        # "gamma current"
-        j_list = [0.0] * (self.num_slices - 1)
+        self.j_up = [0.0] * (self.num_slices - 1)
+        self.j_dn = [0.0] * (self.num_slices - 1)
 
         for i in range(self.num_slices - 1):
-            j_list[i] = - xi * (self.gamma_list[i+1] - self.gamma_list[i]) / self.slice_length;
+            # z derivative
+            gamma_p_i = (self.gamma_list[i+1] - self.gamma_list[i]) / self.slice_length
 
-        # time derivative of gamma
-        gamma_prime = [0.0] * self.num_slices
-        for i in range(1, self.num_slices):
-            gamma_prime[i] += j_list[i - 1] / self.slice_length
-        for i in range(0, self.num_slices - 1):
-            gamma_prime[i] -= j_list[i] / self.slice_length
-        for i in range(0, self.num_slices):
-            gamma_prime[i] -= \
-                self.gamma_list[i] * (1.0 / self.material.ds_up + 1.0 / self.material.ds_dn) / self.material.tau
+            j_up_0_i = -self.material.alpha_up * (self.material.ds_dn / ds_tot) * gamma_p_i
+            j_dn_0_i = +self.material.alpha_dn * (self.material.ds_up / ds_tot) * gamma_p_i
+
+            ee_i = (j_up_0_i + j_dn_0_i + self.j_hot[i]) / alpha_tot
+
+            self.j_up[i] = j_up_0_i - self.material.alpha_up * ee_i
+            self.j_dn[i] = j_dn_0_i - self.material.alpha_dn * ee_i
+
+        # time derivative
+        gamma_dot = [0.0] * self.num_slices
+
+        for i in range(self.num_slices):
+            gamma_dot[i] -= self.gamma_list[i] * (
+                1.0 / self.material.ds_up + 1.0 / self.material.ds_dn
+            ) / self.material.tau
+
+        for i in range(self.num_slices - 1):
+            # gamma_flow is NIET een werkbaar concept als ds_sigma ruimte-afhankelijk is
+            gamma_flow = (self.j_up[i] / self.material.ds_up - self.j_dn[i] / self.material.ds_dn) / self.slice_length
+            gamma_dot[i] -= gamma_flow
+            gamma_dot[i + 1] += gamma_flow
 
         for i in range(0, self.num_slices):
-            self.gamma_list[i] += self.dt * gamma_prime[i]
+            self.gamma_list[i] += self.dt * gamma_dot[i]
 
         # excitation and decay of hot electrons ------------------------------------------------------------------------
 
@@ -167,13 +208,18 @@ class System:
             mu0_up[i] = (+self.gamma_list[i] * self.material.ds_dn - hot_tot[i] / self.slice_length) / (self.material.ds_up + self.material.ds_dn)
             mu0_dn[i] = (-self.gamma_list[i] * self.material.ds_up - hot_tot[i] / self.slice_length) / (self.material.ds_up + self.material.ds_dn)
 
-        return (self.gamma_list.copy(), mu0_up, mu0_dn, mu0_hot_up, mu0_hot_dn)
+        return (self.gamma_list.copy(), mu0_up, mu0_dn, hot_up, hot_dn, mu0_hot_up, mu0_hot_dn, self.j_hot, self.j_up, self.j_dn)
 
     def make_ticks(self):
-        ticks = []
+        ticks_slices = []
         for i in range(self.num_slices):
-            ticks.append(i * self.slice_length)
-        return ticks
+            ticks_slices.append((i + 0.5) * self.slice_length)
+
+        ticks_planes = []
+        for i in range(self.num_slices - 1):
+            ticks_planes.append((i + 1.0) * self.slice_length)
+
+        return (ticks_slices, ticks_planes)
 
     def plot(self):
         ticks = self.make_ticks()
@@ -216,8 +262,8 @@ def make_system():
 
     system.material.ds_up = 70.0  # (eV^-1 nm^-3)
     system.material.ds_dn = 30.0  # (eV^-1 nm^-3)
-    system.material.alpha_up = 8.0  # (eV^-1 nm^-1 fs^-1)
-    system.material.alpha_dn = 2.0  # (eV^-1 nm^-1 fs^-1)
+    system.material.alpha_up = 0.5  # (eV^-1 nm^-1 fs^-1)
+    system.material.alpha_dn = 0.5  # (eV^-1 nm^-1 fs^-1)
     system.material.tau = 10  # (NOT fs)
 
     system.dt = 1.0
